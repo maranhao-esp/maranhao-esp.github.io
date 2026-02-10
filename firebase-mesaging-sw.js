bdcd86ff49f9d09b3ee7b145a7b58a9579f43ade
maranhao-esp <caua60198@gmail.com>
2026-02-10 02:33:04 -0300

    Create firebase-mesaging-sw.js

---
+++ firebase-mesaging-sw.js
@@ -0,0 +1,34 @@
+// firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/9.17.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.17.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAsK3kEuustv7KyoQXR0Ep-whTpDCDa03w",
+  authDomain: "manuu-c23a7.firebaseapp.com",
+  databaseURL: "https://manuu-c23a7-default-rtdb.firebaseio.com",
+  projectId: "manuu-c23a7",
+  storageBucket: "manuu-c23a7.firebasestorage.app",
+  messagingSenderId: "378235951611",
+  appId: "1:378235951611:web:72c855fbb5ccdb1a84535e"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[SW] Push recebido em background:', payload);
+
+  const title = payload.notification?.title || 'Mensagem de Nosso Mundo ❤️';
+  const options = {
+    body: payload.notification?.body || 'Alguém mandou algo especial!',
+    icon: 'https://cdn-icons-png.flaticon.com/512/2589/2589175.png',
+    badge: 'https://cdn-icons-png.flaticon.com/512/2589/2589175.png',
+    data: { url: '/' }
+  };
+
+  self.registration.showNotification(title, options);
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow(event.notification.data.url || '/'));
+});
